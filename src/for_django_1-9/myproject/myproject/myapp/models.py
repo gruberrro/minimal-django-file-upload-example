d60105d75cad2d89d30d6835a28fe3351268a3c7
Mateusz <juniorjozek@o2.pl>
2017-07-28 10:59:53 +0200

    Typo in the path

--- src/for_django_1-9/myproject/myproject/myapp/models.py
+++ src/for_django_1-9/myproject/myproject/myapp/models.py
@@ -7,4 +7,4 @@ from django_antivirus_field import ProtectedFileField
 class Document(models.Model):
     # instead of using FileField use ProtectedFileField
     # docfile = models.FileField(upload_to='documents/%Y/%m/%d')
-    docfile = ProtectedFileField(upload_to='document/%Y/%m/%d')
+    docfile = ProtectedFileField(upload_to='documents/%Y/%m/%d')
